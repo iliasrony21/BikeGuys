5b76d0588756723e285c6461fd1c81e6ec957fb8
iliasrony21 <iliasronywe21@gmail.com>
2022-12-02 01:11:26 +0600

    run

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,24 +1,31 @@
 // import { EmailJSResponseStatus } from '@emailjs/browser'
 import React, { useRef, useState } from 'react'
-import emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/browser'
 import './Contact.css'
 import svg from '../../images/contact.svg'
 const Contact = () => {
-  const form = useRef();
-  
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const form = useRef()
 
-    emailjs.sendForm('service_882ykyt', 'template_1ygevew', form.current, 'qPuueJOpW7Qo9Hhf2')
-      .then((result) => {
-          console.log(result.text);
+  const sendEmail = e => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_882ykyt',
+        'template_1ygevew',
+        form.current,
+        'qPuueJOpW7Qo9Hhf2'
+      )
+      .then(
+        result => {
+          console.log(result.text)
           alert('Successfully send the mail')
-          
-      }, (error) => {
-          console.log(error.text);
-      });
-      
-  };
+        },
+        error => {
+          console.log(error.text)
+        }
+      )
+  }
   return (
     <div className='flex'>
       <div className='contact1 mt-20'>
@@ -27,7 +34,12 @@ const Contact = () => {
       <div className='contact2'>
         <div className='fullPart mt-20 ml-10 '>
           <div className='right-sided '>
-            <form ref={form} onSubmit={sendEmail} action='' className='flex flex-col gap-5'>
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              action=''
+              className='flex flex-col gap-5'
+            >
               <h1 className='text-center text-white text-4xl '>Contact US </h1>
               <input
                 type='text'
@@ -46,7 +58,7 @@ const Contact = () => {
                 name='message'
                 placeholder='Message'
               ></textarea>
-              <button className='btn bg-white text-black text-center rounded-full w-32 ml-32 mb-5'>
+              <button className='btn bg-white text-black border-none text-center rounded-full w-32 ml-32 mb-5'>
                 Send
               </button>
               {/* {/* <span className='text-green-600 text-center text-2xl'> */}
